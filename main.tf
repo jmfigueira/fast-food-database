444552dc6c44bb39fb74a414efaa4f234531b6c4
hugocesarcoelho <hugo_cesar45@hotmail.com>
2025-03-21 11:45:52 -0300

    feat: adding changes

--- main.tf
+++ main.tf
@@ -1,3 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "hugo-organization"
+
+    workspaces {
+      name = "learn-terraform-github-actions"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_pet" "sg" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "random_password" "db_password" {
   length  = 16
   special = false
